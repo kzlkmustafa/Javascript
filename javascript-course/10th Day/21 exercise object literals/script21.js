c2ff2e28a81a98cc1f029bb56ed6e5e09039530c
Mustafa <102860195+kzlkmustafa@users.noreply.github.com>
2022-05-16 11:11:29 +0300

    added 10th day

---
+++ javascript-course/10th Day/21 exercise object literals/script21.js
@@ -0,0 +1,63 @@
+var aracBilgileri = {
+    id : "bmw116d_123",
+    model : "bmw 116d",
+    yil : 2015,
+    renk : "Beyaz",
+    servisKayitlari : [
+        {
+            id : "bmw116d_123_1",
+            tarih : "20.01.2016",
+            km : "13000",
+            ucret : 900,
+            detay : [
+                {
+                    id : "bmw116d_123_1_1",
+                    aciklama : "yag degisimi ücret",
+                    ucret : 300
+                },
+                {
+                    id : "bmw116d_123_1_2",
+                    aciklama : "filtre degisimi ücret",
+                    ucret : 300
+                },
+                {
+                    id : "bmw116d_123_1_3",
+                    aciklama : "fren hidroliği",
+                    ucret : 300
+                },
+            ]
+        },
+        {
+            id : "bmw116d_123_2",
+            tarih : "20.01.2016",
+            km : "28000",
+            ucret : 1800,
+            detay : [
+                {
+                    id : "bmw116d_123_2_1",
+                    aciklama : "yag degisimi ücret",
+                    ucret : 350
+                },
+                {
+                    id : "bmw116d_123_2_2",
+                    aciklama : "filtre degisimi ücret",
+                    ucret : 350
+                },
+                {
+                    id : "bmw116d_123_2_3",
+                    aciklama : "fren hidroliği",
+                    ucret : 300
+                },
+                {
+                    id : "bmw116d_123_2_4",
+                    aciklama : "balata değişimi",
+                    ucret : 800
+                }
+            ]
+        }
+    ]
+}
+
+
+console.log(aracBilgileri.id);
+
